bacf353fc64569afe44fb3b42566c8964663f386
ChrisSasias <christophersasias@hotmail.com>
2023-08-15 01:36:53 -0300

    bucles process

---
+++ probando.js
@@ -0,0 +1,16 @@
+let suma = 0;
+
+// for(let i =0; i <5; i++){
+
+//     suma = suma + i;
+//     console.log("variable de iteracion:" + i);
+
+
+// }
+// console.log('variable Suma:', suma);
+
+while(suma < 3){
+
+    suma=suma +1;
+    console.log(suma);
+}
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+for(let i =0; i <5; i++){
+
+//     suma = suma + i;
+//     console.log("variable de iteracion:" + i);
+
+
+// }
+// console.log('variable Suma:', suma);
